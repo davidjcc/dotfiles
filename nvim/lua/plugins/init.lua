4202b7f86f94979e1d156746aa77a5c518dc3706
David Connett <davidcon90@gmail.com>
2024-02-25 19:59:06 +0000

    Added incline.nvim plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,31 @@ return {
     -- Add indentation guides TODO: Make the lines darker.
     -- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
+    -- Add floating status lines to buffers.
+    {
+        'b0o/incline.nvim',
+        opts = {
+            window = {
+                padding = 0,
+                margin = { horizontal = 0 },
+            },
+            render = function(props)
+                local helpers = require("incline.helpers")
+                local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+                local ft_icon, ft_color = require('nvim-web-devicons').get_icon_color(filename)
+                local modified = vim.bo[props.buf].modified
+                local buffer = {
+                    ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+                    ' ',
+                    { filename, gui = modified and 'bold,italic' or 'bold' },
+                    ' ',
+                    guibg = '#44406e',
+                }
+                return buffer
+            end,        },
+        -- Optional: Lazy load Incline
+        event = 'VeryLazy',
+    },
     -- Fun utility.
     {
         'Eandrju/cellular-automaton.nvim',
@@ -25,7 +50,6 @@ return {
         dependencies = "nvim-treesitter/nvim-treesitter",
         config = true,
         -- Uncomment next line if you want to follow only stable versions
-        -- version = "*" 
     },
 
     -- Search and replace panel
