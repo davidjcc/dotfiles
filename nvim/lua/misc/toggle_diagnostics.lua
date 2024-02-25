de357e691c3863813087708a6f24753edad533d0
David Connett <davidcon90@gmail.com>
2024-02-25 20:48:18 +0000

    Add print statement when toggling diagnostics
    You don't currently know the status of the diagnostics toggle, adding a print will help.

--- nvim/lua/misc/toggle_diagnostics.lua
+++ nvim/lua/misc/toggle_diagnostics.lua
@@ -1,11 +1,14 @@
 function ToggleDiagnostics()
   if vim.g.diagnostics_enabled == nil then
     vim.g.diagnostics_enabled = false
+    print("Diagnostics disabled")
     vim.diagnostic.disable()
   elseif vim.g.diagnostics_enabled then
+    print("Diagnostics disabled")
     vim.g.diagnostics_enabled = false
     vim.diagnostic.disable()
   else
+    print("Diagnostics enabled")
     vim.g.diagnostics_enabled = true
     vim.diagnostic.enable()
   end
